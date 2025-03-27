4956260a6ba9833e40418267268fdbfd664d42ff
meyigi <faybmi7@gmail.com>
2025-03-27 14:54:29 +0600

    Java Swing - Frames

---
+++ java-swing/frame/src/App.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        MyFrame frame = new MyFrame();
+    }
+}
---
+++ java-swing/frame/src/MyFrame.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+    MyFrame() {
+        this.setTitle("Hello world!");
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(420, 420);
+        this.setVisible(true);
+        ImageIcon image = new ImageIcon("logo.png");
+        System.out.println(image.getImage());
+        this.setIconImage(image.getImage());
+        this.getContentPane().setBackground(Color.green);
+    }
+}
