3706887f7cc2121042416d364aebe2f92019c3f6
MeyiGi <faybmi7@gmail.com>
2025-11-15 17:16:27 +0600

    database

---
+++ Oracle-SQL/week1_start (Copy).sql
@@ -0,0 +1,83 @@
+// database security can be classified in two system and data
+// system access to data and use of data like username and password
+// data security securing the data inside the table inside the database
+
+// priviliges it means write in sql to particular ....
+// system priviliges performing particular action in database
+// object privilieges manipulate content of object in database
+// database administrator have all priviligies, also can give priviligies to the user, user can give priviliges to another users and so on
+// schemas is a collection of objects owned by database user
+
+// there more than 200 privilieges are available system privilieges
+// tasks such as creating, removing users removing backup tables
+
+// SYSTEM_PROVILAGE_MAP we can see different types of privilieges
+
+// creating user
+CREATE USER user
+IDENTIFIED BY password; 
+
+CREATE USER demo
+IDENTIFIED BY demo // this demo user do not have any priviligies, password is case sensitive
+// TO GRANT priviliges
+GRANT privilege [, privilege...]
+TO USER [, user] role, PUBLIC...] // with PUBLIC we mean giving privilige to all users
+// type of priviligies examples
+CREATE SESSION
+CREATE TABLE
+CREATE TABLE CREATE SEQUENCE
+CREATE VIEW
+CREATE PROCEDURE
+
+GRANT   create session, create table, 
+        create sequence, create view 
+TO demo
+// DBA stands for database administrator
+// difference between role and user 
+// role is group of user for example "Manager" and we give privilige to manager all managers will grant privilege, we can use several roles users can be assigned to several roles
+
+CREATE ROLE manager
+GRANT create table, create view
+TO MANAGER
+
+GRANT MANAGER TO  alice // what if we make operation with manager but nobory is granted to manager role
+
+// Changing passowrd
+ALTER USER demo
+IDENTIFIED BY new_password // can I use numbers
+
+// object privileges: ALTER, DELETE, INDEX, INSERT, REFERENCES(foreighn key, if we provide REFERENCES privilege it means user can work with FOREIGN KEY), SELECT, UPDATE
+
+
+GRANT   object_priv[(columns)]
+ON      ojbect // we are giving privelege to this object
+TO      (user|role|PUBLIC)
+[WITH GRANT OPTION]; // to grant a object to the other users
+
+GRANT UPDATE (DEPARTMENT_NAME, location_id)
+ON            departments
+TO            demo, manager;
+
+GRANT select, insert
+ON departments
+TO demo
+WITH GRANT OPTION; // demo can give this privelege to other users
+
+GRANT select
+ON alice.departments // departments table in alice user
+TO PUBLIC // all user can use select to alice.departments table, PUBLIC assess can give only DBA
+
+
+REVOKE [privilege [, privilege...]|ALL]
+ON object
+FROM {user[, user...]|role|PUBLIC}
+[CASCADE CONSTRAINTS] // when we remove grant from one user that granted users with this user will be revoked like CASCADE in foreigh key it is good when we use it
+// maybe I am wrong I need research it
+
+REVOKE select, insert
+ON departments
+FROM demo // we are removing select and insert privelegies from demo user
+// WITH ADMIN OPTION --> if we use this clause we are giving system privelege we need to be very careful using it sooooooo in such case two DBA?
+
+quiz a. true, b. false[because only DBA can create role], c. true, d. true 
+
--- Oracle-SQL/week6_objects.sql
+++ Oracle-SQL/week6_objects.sql
@@ -2,15 +2,38 @@
 // Advantages of views: restricting data access to physical table, to make complex queries easy, ... [another two ones] 
 // simple views contain one, do not contain function, groups does not present, DML
 // complext views, contain one or more, contain functionsm grups, do not contain DML
-CREATE VIEW empvu80
+CREATE OR REPLACE VIEW empvu80
+AS  SELECT employee_id, last_name, salary FROM Employees
+    WHERE department_id = 80;
+
+    
+    
+DESCRIBE empvu80
+SELECT COUNT(*) FROM empvu80 // inserted or removed in physical table will affect view
+
+INSERT INTO Employees
+
+CREATE OR REPLACE VIEW empvu80 (id_number, hoho, hokotok, dsa) // if number of alias more than SELECT columns it will cause an error
+AS  SELECT employee_id, last_name, salary FROM Employees
+    WHERE department_id = 80;
+CREATE OR REPLACE VIEW empvu80 (id_number, hoho) // same as before it will cause an error
 AS  SELECT employee_id, last_name, salary FROM Employees
     WHERE department_id = 80;
 DESCRIBE empvu80
 
-CREATE OR REPLACE VIEW salvu50 (ID_NUMBER, NAME, ANN_SALARY)
+UPDATE empvu80
+SET salary = salary + 5000 // can be modified because empvu80 is simple view
+
+// name of columnns, CREATE OR REPLACE allows us to recreate view if it hadn't be there it would cause an error
+CREATE OR REPLACE VIEW salvu50 (ID_NUMBER, NAME, ANN_SALARY) 
 AS  SELECT employee_id, last_name, salary * 12 FROM Employees
-    WHERE department_id = 50
-DESCRIBE salvu50
+    WHERE department_id = 50;
+DESCRIBE salvu50    
+
+UPDATE salvu50
+SET salary = salary / 12 // it won't work because there expression salary * 12
+
+
 
 SELECT * FROM salvu50 // to retrieve data from VIEWS
 // in order to modify view we need add "CREATE OR REPLACE" to the VIEW
@@ -39,9 +62,18 @@ AS  SELECT department_id, MIN(salary)
     
 DELETE FROM empvn
 WHERE department_id = 100 // why not department???
+
+CREATE OR REPLACE VIEW empv AS
+SELECT DISTINCT(department_id), first_name FROM Employees
+CREATE OR REPLACE VIEW empv (hoho, kotok) AS 
+SELECT first_name, salary * 12 FROM Employees
+SELECT * FROM empv
+DELETE FROM empv
+WHERE department_id = 90 // not working
+// we can not use remove DML operation on JOIN, GROUP, ROWNUM and AGGREGATE FUNCTIONS
 // can not be removed rows because it contain several tables, you can imagine it as trying remove from one physical table, it is impossible because data in another table do not present
-// what if we add new rows will it show new rows in view?
-// how we can remove VIEW?
+// what if we add new rows will it show new rows in view? yes
+// how we can remove VIEW? DROP view view
 CREATE OR REPLACE VIEW empvn1
 AS  SELECT DISTINCT first_name, employee_id
     FROM employees // it is not simple VIEW because we have DISTINCT we can not determine particular row
@@ -52,6 +84,10 @@ AS  SELECT employee_id, last_name
     FROM employees
     WHERE ROWNUM <= 3 // ROWNUM counting as complext VIEW thereby we can not remove rows in VIEW, why? 
 
+
+// group functions, GROUP BY, JOIN, DISTINCT, ROWNUM --> Can not be implemented REMOVE
+// group functions, GROUP BY, JOIN, DISTINCT, ROWNUM, expression --> Can not be implemented MODIFY
+// group functions, GROUP BY, JOIN, DISTINCT, ROWNUM, expression, NOT NULL in TABLE --> Can not be implemented ADD
 
 SELECT employee_id, last_name
 FROM employees
@@ -72,7 +108,7 @@ AS  SELECT
 // how I can add another type of CONTRAINT???
 
 // when we add READ_ONLY it will forbid using DML statements but it is saying in presenations SELECT in DML operations ???? -> it turns out to lie SELECT statement is DQL statement (data querying language)
-// can I use simultaneously WITH CHECK OPTION and READ ONLY in VIEW
+// can I use simultaneously WITH CHECK OPTION and READ ONLY in VIEW --> No
 // fucking FORCE and NOFORCE what does it mean in VIEW?
 
 
@@ -88,4 +124,92 @@ SELECT * FROM names // what the difference between WITH and VIEWS?
 // DEFINITION: sequence - ????
 // DEFINITION: index - to improve performance data retrieval process
 // DEFINITION: synonym - gives alternative name to objects
+// DEFINITION: sequence is like automatic generator of nubmers unique primary key
+
+
+// 
+CREATE SEQUENCE sequence
+    [INCREMENT BY N]
+    [START WITH N]
+    [(MAXVALUE N | NOMAXVALUE)] // 10^26
+    [(MINVALUE N | NOMINVALUE)] // if we do not use minvalue it is nominvalue by default
+    [(CYCLE | NOCYCLE)] // it enables generation process even if all used it is like circular array
+    [(CACHE N | NOCACHE)]; // cache mean how many can be saved in cache N quantity
+    
+CREATE SEQUENCE dept_deptid_seq 
+    INCREMENT BY 10
+    START WITH 120
+    MAXVALUE 9999 // WHAT IF WE DIDN'T SPECIFY? it will be 10^26
+    NOCACHE
+    NOCYCLE // Why we need use this? we must to keep all parameters
+    
+// how to use sequence?? By using Pseudocolumns
+// NEXTVAL -> returns the next available sequence value
+// CURRVAL -> returns the current value
+
+INSERT INTO departments(department_id, departent_name, location_id) 
+VALUES (dept_deptid_seq.nextval, 'support', 2500)
+    
+// if we want to insert a row but max length is less? what will happen? it will cause error in such case we need modify sequence MAXVALUE
+// if system crases or rollback occurs in sequence, that sequence will be lost [rollback if there was 100 at the currval it will be removed and nextval will be 101 not optimal]
+// they are like pointers
+// what if we use sequence in two or more tables what will happen? it is bad but applicable
+
+// to modify sequence we are using ALTER
+
+ALTER SEQUENCE dept_deptid_seq
+    INCREMENT BY 20
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE // what if I change positions NOCACHE <-> NOCYCLE what happens? we must ensure all values in right places
+
+// we can not change START WITH part by using ALTER SEQUENCE statement we have only recreating sequence option for that
+// what if we update MAXVALUE to less one? error 100 -> 99 
+// here is dropping statemtn
+DROP SEQUENCE dept_deptid_seq;
+
+// DEFINITION: index object - is database object to improve performance some queries is a schema object can be used by Oracle BY using pointer dependent on a table, can be maintenad automatically by Oracle or by User end
+// how oracle table does the search -> with index and without: by using index it will find wor_id and directly retrieve data in contrast when we retriving value without index it will look at them one by one, so indexes much better by using binary search etc directly retrieving data
+// we can imagine it as book pages with their page numbers or without them
+// it can be created or dropped in any time and will not affect table, we are adding indexes to specific column we are not creating new column
+// we can create it with two way implicitly or explicitly 
+// implicitly unique index created automatically when we define PRIMARY KEY or UNIQUE
+CREATE [UNIQUE][BITMAP]INDEX index // unique means uniques, bitmap used for low unique values count like gender
+ON table (column[, column]...);
+
+CREATE INDEX emp_last_name_idx
+ON employees(last_name);
+
+
+// must we put index to each columns if it is so useful?
+// we need use index if contains large range of value or a lot null values or one or more columns used frequently on WJERE claue or a join
+
+// we do not need use INDEX for column that a little bit used
+// table is small or updated frequently or the indexed columns are referenced as part of an expression[like expressions salary * 12 / 23 + 2134]
+// in order to remove index we are using our loving DROP
+DROP INDEX index
+DROP INDEX emp_last_name_idx; // to drop an index we must be the owner of the index or have the DROP ANY INDEX privilage, we can not update indexes
+// when we made DML operation we have to make updating table it is bad situationship for table
+
+
+// DEFINITION: synonym - in order to give alternative name to the object
+// why we need it? we know in sql there can be different users,i want retrieve data for different users table for example we wanna retrieve Employees with another user
+// it creates another name for an object 
+CREATE [PUBLIC] SYNONYM synonym // PUBLIC means assecible to all users
+FOR object; 
+// instead os saying Bob.departments we can create a synonym bdep also it will hep shorten lenghy object names
+
+CREATE SYNONYM d_sum
+FOR dept_sum_vu; // we are renaming dept_sum_vu and can use it, can be used for all objects
+
+CREATE SYNINYM a_dept
+FOR alice.departments
+
+// quiz 1. False
+
+// 
+
+
+
+
 
